56c466bd8ba3c0a8c9934fb532b7c6b3fc1ffca1
examplu2026-maker <examplu2026@gmail.com>
2026-09-09 14:10:47 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@
 // Who has access: Anyone → copy the URL it gives you.
 // ============================================================
 
-const API_URL = "https://script.google.com/macros/s/AKfycbwVU0SQrAgXslODrcdU53HC2XqZz9l6OmvjCjmsx2--D1E9rfUY2r8A2KPRPp4KuyS3xg/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyXEfobbn_1LqIageDpk-DRd14jqvn6VMmCtuthrR2gTh-hOSgg55qkSlmuRqIlkzfXiQ/exec";
 
